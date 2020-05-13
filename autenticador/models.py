136112aed23c94f463144d99dc39dd8cafc0d812
carlos <wixancl@gmail.com>
2020-05-13 16:37:29 -0400

    first commit

---
+++ autenticador/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Dispositivos(models.Model):
+    nombre=models.CharField(max_length=30,default="Nombre")
+    modelo=models.CharField(max_length=30, null=True)
+    descripcion=models.CharField(max_length=30, null=True)
+    token=models.CharField(max_length=50,null=True)
+    activo= models.BooleanField(default=1)
+    #fechacreacion=models.DateField()
